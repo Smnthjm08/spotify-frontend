8e6645deb437f5b89c614a0e49729198fdd35624
sumanth <119963915+Smnthjm08@users.noreply.github.com>
2025-01-15 12:19:28 +0530

    Revert " Revert UI Design, Fix Sidebar, and Setup Refresh Token Auth  "

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
 import { Route, Routes } from "react-router-dom";
-import HomePage from "./components/home";
 import LoginPage from "./app/(auth)/login-page";
 import RegisterPage from "./app/(auth)/register-page";
 import ForgotPasswordPage from "./app/(auth)/forgot-password";
 import VerifyEmailPage from "./app/(auth)/verify-email";
 import ResetPasswordPage from "./app/(auth)/reset-password";
 import AppContainer from "./components/app-continer";
-import DashBoard from "./components/dashboard";
+import MainLayout from "./app/dashboard/page";
+
+// import Home from "./components/home";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<AppContainer />}>
-        <Route index element={<HomePage />} />
-        <Route path="dashboard" element={<DashBoard />} />
+        {/* <Route index element={<Home />} /> */}
+        <Route index element={<MainLayout />} />
       </Route>
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
--- src/components/app-continer.tsx
+++ src/components/app-continer.tsx
@@ -1,22 +1,30 @@
 import useAuth from "@/hooks/use-auth";
-import { Navigate, Outlet } from "react-router-dom";
+import { Navigate } from "react-router-dom";
+import MainLayout from "@/app/dashboard/page";
 
-const AppContainer = () => { 
+const AppContainer = () => {
   const { user, isLoading } = useAuth();
 
   return (
     <div>
-      {isLoading ? (
-        <div>Loading...</div>
-      ) : user ? (
-        <Outlet /> // Renders child routes
-      ) : (
-        <Navigate 
-          replace 
-          to="/login" 
-          state={{ redirectUrl: window.location.pathname }} 
-        />
-      )}
+      <div>
+        {isLoading ? (
+          <div>loading...</div>
+        ) : user ? (
+          <div>
+            <MainLayout />
+          </div>
+        ) : (
+          <Navigate
+            replace
+            to="/login"
+            state={{ redirectUrl: window.location.pathname }}
+          />
+          // this state is used to pass the redirect url to
+          // the login page which helps in redirecting the
+          // user to the previous page after login is succesful.
+        )}
+      </div>
     </div>
   );
 };
