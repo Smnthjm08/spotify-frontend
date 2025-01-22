bdc56c4b4b262732582944586827ee79848030fb
Smnthjm08 <sumanthjmm.04@gmail.com>
2025-01-22 23:19:09 +0530

    section grid for home page

--- src/components/cards/made-for-you-card.tsx
+++
@@ -1,7 +0,0 @@
-export default function MadeForYouCard() {
-    return (
-        <div>
-            <div>Made for you</div>
-        </div>
-    )
-}
--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,19 +1,42 @@
 import useAuth from "@/hooks/use-auth";
 import TopBar from "./global/top-bar";
-import MadeForYouCard from "./cards/made-for-you-card";
 import FeaturedCard from "./cards/featured-grid-card";
+import { ScrollArea } from "./ui/scroll-area";
+import SectionGrid from "./cards/section-grid";
+import { getMadeForYouSongsRequest, getTrendingSongsRequest } from "@/lib/api";
+import { useQuery } from "@tanstack/react-query";
+
+const MADEFORYOU = "made-for-you";
+const TRENDING = "trending";
 
 const HomePage = () => {
   const { user } = useAuth();
 
+  const { data: madeForYouSongs } = useQuery({
+    queryKey: [MADEFORYOU],
+    queryFn: getMadeForYouSongsRequest,
+  });
+
+  const { data: trendingSongs } = useQuery({
+    queryKey: [TRENDING],
+    queryFn: getTrendingSongsRequest,
+  });
+
   return (
-    <div>
+    <main className="rounded-md overflow-hidden h-full ¯bg-gradient-to-b from-zinc-800 to-zinc-900">
       <TopBar user={user} />
-      <div className="flex items-center font-semibold justify-center gap-4 min-h-screen">
-        <FeaturedCard />
-        <MadeForYouCard />
-      </div>
-    </div>
+      <ScrollArea className="h-[calc(100vh-180px)]">
+        <div className="p-4 sm:p-6 ">
+          <h1 className="text-2xl sm:text-3xl font-bold mb-6">Good Morning</h1>
+          <FeaturedCard />
+
+          <div className="space-y-8">
+            <SectionGrid title="Made For You" songs={madeForYouSongs} />
+            <SectionGrid title="Trending" songs={trendingSongs} />
+          </div>
+        </div>
+      </ScrollArea>
+    </main>
   );
 };
 
